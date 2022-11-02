f17439aedda24136032c72ebbd68728bf6794e26
Iyobosa Omoruyi <omosiyobo@gmail.com>
2022-11-02 15:46:43 +0100

    basic express server setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require ("express");
+const dotenv = require ("dotenv");
+const mongoose = require ("mongoose");
+const cors = require ("cors");
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({extended:true}));
+app.use(express.urlencoded({extended:true}));
+
+app.use(cors());
