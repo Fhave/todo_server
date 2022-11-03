6f9d69242369882ac86ccf4989367448b3b8b99e
Iyobosa Omoruyi <omosiyobo@gmail.com>
2022-11-03 13:59:22 +0100

    mongodb setup

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ app.use(express.json({extended:true}));
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors());
+
+const mongodb = "mongodb+srv://test:test1234@cluster1.wvnchbt.mongodb.net/mern_todo_db?retryWrites=true&w=majority";
+
+
+const PORT = process.env.PORT || 3000;
+mongoose.connect(mongodb,
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+  }
+);
+
+app.get('/', (req,res) => {
+  res.send('Trial')
+})
+
+
+app.listen(port, function() {
+  console.log(`Listening on port ${PORT}`);
+});
