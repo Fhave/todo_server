6a7878b19cef6c25fbbc651d2c7424be8f066ffc
Iyobosa Omoruyi <omosiyobo@gmail.com>
2022-11-08 01:01:33 +0100

    controllers added

---
+++ controllers/todos.js
@@ -0,0 +1,21 @@
+const mongoose = require ("mongoose");
+const Todo = require('./../models/todos');
+
+export const readTodos = async(req,res) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(404).json({err: err.message})
+  }
+}
+
+export const createTodos = async(req,res) => {
+  const todos = new Todo(req.body);
+  try {
+    await Todo.save();
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(409).json({err: err.message})
+  }
+}
